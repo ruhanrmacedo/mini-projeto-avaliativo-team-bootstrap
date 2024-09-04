2775c2b5e9621ddca4c027e634b75a1ced671f42
Laís Becker <141473728+laisbecker@users.noreply.github.com>
2024-09-03 22:55:33 -0300

    Estilização dos botões e dos cards de laboratório e remédio

--- script/listar_medicamentos.js
+++ script/listar_medicamentos.js
@@ -11,17 +11,30 @@ function listarMedicamentos(filtroLaboratorio = null) {
     remediosFiltrados.forEach((remedio) => {
         const card = document.createElement('div');
         card.className = 'card text-center mb-3';
-        card.style.width = '18rem';
+        card.style.width = '15rem';
+        card.style.border = '1px solid #F5F5F5'; 
+        card.style.boxShadow = 'none'; 
         card.innerHTML = `
             <img src="${remedio.fotoMedicamento}" class="card-img-top" alt="${remedio.nomeMedicamento}">
-            <div class="card-body">
-                <h5 class="card-title">${remedio.nomeMedicamento}</h5>
+            <div class="card-body" style="padding: 0.5rem;">
+                <h5 class="card-title" style="margin-bottom: 0; color: #229799";>${remedio.nomeMedicamento}</h5>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">${remedio.nomeLaboratorio}</li>
-                <li class="list-group-item">R$ ${remedio.preco}</li>
-            </ul>
-            `;
+            <div class="card-footer p-0" style="border-top: 1px solid #F5F5F5";>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item border-0" style="color: #229799";>${remedio.nomeLaboratorio}</li>
+                    <li class="list-group-item border-0 font-weight-bold" style="color: #229799";>R$ ${remedio.preco}</li>
+                </ul>
+            </div>
+        `;
+
+        card.addEventListener('mouseover', function () {
+            card.classList.add('shadow-sm', 'bg-light');
+        });
+
+        card.addEventListener('mouseout', function () {
+            card.classList.remove('shadow-sm', 'bg-light');
+        });
+
         listarMedicamentos.appendChild(card);
     });
 }
@@ -41,14 +54,22 @@ function listarLaboratorios() {
 
     // Item para listar todos os medicamentos
     const allItem = document.createElement('a');
-    allItem.className = 'nav-link text-center';
+    allItem.className = 'nav-link text-center rounded-circle';
     allItem.style.width = '8rem';
     allItem.href = '#';
     allItem.innerHTML = `
-    <img src="https://media.glassdoor.com/sqll/2818519/clamed-farm%C3%A1cias-squarelogo-1584693174740.png" class="rounded-circle mb-1" style="width: 5rem; height: 5rem;" alt="Todos">
+    <img src="https://media.glassdoor.com/sqll/2818519/clamed-farm%C3%A1cias-squarelogo-1584693174740.png" class="rounded-circle mb-1 border p-2 mb-2" style="width: 5rem; height: 5rem;" alt="Todos">
     <br>
     <span>Todos</span>
     `;
+    allItem.style.color = '#48CFCB'; 
+    allItem.style.fontWeight = '500';
+    allItem.addEventListener('mouseover', function () {
+        allItem.style.color = '#229799'; 
+    });
+    allItem.addEventListener('mouseout', function () {
+        allItem.style.color = '#48CFCB';
+    });
     allItem.onclick = function () {
         listarMedicamentos();
     };
@@ -65,13 +86,24 @@ function listarLaboratorios() {
         const imgUrl = laboratorios[nome] ? laboratorios[nome] : 'https://media.glassdoor.com/sqll/2818519/clamed-farm%C3%A1cias-squarelogo-1584693174740.png';
 
         navItem.innerHTML = `
-        <img src="${imgUrl}" class="rounded-circle mb-1" style="width: 5rem; height: 5rem;" alt="${nome}">
+        <img src="${imgUrl}" class="rounded-circle mb-1 border p-2 mb-2" style="width: 5rem; height: 5rem;" alt="${nome}">
         <br>
         <span>${nome}</span>
         `;
         navItem.onclick = function () {
             filtrarPorLaboratorio(nome);
         };
+
+        navItem.style.color = '#48CFCB'; 
+
+        navItem.style.fontWeight = '500';
+
+        navItem.addEventListener('mouseover', function () {
+            navItem.style.color = '#229799';
+        });
+        navItem.addEventListener('mouseout', function () {
+            navItem.style.color = '#48CFCB';
+        });
 
         navLaboratorios.appendChild(navItem);
     }
